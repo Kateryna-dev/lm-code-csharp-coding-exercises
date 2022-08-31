f16566aac05a3b019d569b2123c55d28e7879eee
Kateryna <kateryna.dev@gmail.com>
2022-08-31 08:36:58 +0100

    Exercise  2 optimization

--- Exercises/Exercise002.cs
+++ Exercises/Exercise002.cs
@@ -9,24 +9,12 @@ namespace Exercises
 
         public bool IsFromManchester(Person person)
         {
-            if (person == null) 
-            {   
-                return false;
-            }
-
-            // Will retun true if person is from Manchester. Otherwise false
-            return person.City == MANCHESTER;
+            return person != null && person.City == MANCHESTER;
         }
 
         public bool CanWatchFilm(Person person, int ageLimit)
         {
-            if (person == null) 
-            {
-                return false;
-            }
-
-            // Will retun true if age of person is equal or bigger than limit. Otherwise false
-            return person.Age >= ageLimit;
+            return person!= null && person.Age >= ageLimit;
         }
     }
 }
